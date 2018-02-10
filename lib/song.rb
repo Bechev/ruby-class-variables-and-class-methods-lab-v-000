5c1d3d866fae2d2b0f3f0d50b9f631a3abe0bd91
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-10 02:29:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,10 +3,25 @@ class Song
   @@count = 0
   @@artists =[]
   @@genres=[]
+  
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@count +=1
+    @@artists << artists
+    @@genres << genre
   end
 
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists
+  end
+
+  def self.genres
+    @@genres
+  end
 end
